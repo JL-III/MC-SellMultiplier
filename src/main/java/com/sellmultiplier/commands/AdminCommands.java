75dd553d1185cd96cb9782729542bd280f09df98
JL-III <jesselopez1162@gmail.com>
2023-05-16 09:57:26 -0500

    build prototype for stacking/counting ocurrences of sell multiplier

--- src/main/java/com/sellmultiplier/commands/AdminCommands.java
+++ src/main/java/com/sellmultiplier/commands/AdminCommands.java
@@ -3,11 +3,17 @@ package com.sellmultiplier.commands;
 import com.sellmultiplier.managers.ConfigManager;
 import com.sellmultiplier.managers.MultiplierManager;
 import com.sellmultiplier.managers.PermissionsManager;
+import com.sellmultiplier.utils.GeneralUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AdminCommands implements CommandExecutor {
 
@@ -29,8 +35,31 @@ public class AdminCommands implements CommandExecutor {
         }
 
         if (args.length == 0 || !"reload".equalsIgnoreCase(args[0])) {
+            Set<PermissionAttachmentInfo> perms = player.getEffectivePermissions();
+            GeneralUtils.log("Permissions for " + player.getName());
+
+            // Regex pattern to match 'sell.multiplier.*'
+            Pattern pattern = Pattern.compile("^sell\\.multiplier\\..*");
+
+            // Counter for occurrences
+            int count = 0;
+            for (PermissionAttachmentInfo perm : perms) {
+                String permission = perm.getPermission();
+                GeneralUtils.log(permission);
+
+                // Check if permission matches pattern
+                Matcher matcher = pattern.matcher(permission);
+                if (matcher.find()) {
+                    count++;
+                }
+            }
+
+            // Log the count
+            GeneralUtils.log("Count of 'sell.multiplier.*' permissions: " + count);
+
             return false;
         }
+
 
         configManager.reloadConfig();
         permissionsManager.loadMultiplierPerms();
