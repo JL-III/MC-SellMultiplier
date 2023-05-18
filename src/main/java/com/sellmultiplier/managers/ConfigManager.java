cfba4fe955e22db3547f71afbc39bb79ac961dd2
Jesse Lopez <jesselopez1162@gmail.com>
2023-05-17 21:56:30 -0500

    revert change in configManager, need to look more closely at functionality

--- src/main/java/com/sellmultiplier/managers/ConfigManager.java
+++ src/main/java/com/sellmultiplier/managers/ConfigManager.java
@@ -29,53 +29,29 @@ public class ConfigManager {
         // By default, the multiplier is 1
         BigDecimal multiplier = BigDecimal.ONE;
 
-        // Check if multiplier stacking is enabled
-        if (multiplierStacking.getBoolean("enabled")) {
-            GeneralUtils.log("Using multiplier stacking");
-
-            // Get the base value from the config
-            String multiplierString = multiplierStacking.getString("base-value");
-            if (multiplierString == null) {
-                GeneralUtils.logWarning("No base-value specified in config! Defaulting to 1");
-                return multiplier;
-            }
-            try {
-                // Try to parse the multiplier string as a BigDecimal
-                multiplier = BigDecimal.valueOf(Double.parseDouble(multiplierString));
-                GeneralUtils.log("Base value is " + multiplier);
-            } catch (Exception e) {
-                // If the parsing fails, log a warning and return a default multiplier
-                GeneralUtils.logWarning("Invalid multiplier '" + multiplierString + "' specified for '" + name + "'. Defaulting multiplier to 1.");
-                return multiplier;
-            }
-            return multiplier;
-        } else {
-            // If multiplier stacking is not enabled
-
-            // Check if sell multipliers are configured
-            if (sellMultipliers == null) {
-                GeneralUtils.logWarning("No sell-multipliers specified in config! Defaulting to 1");
-                return multiplier;
-            }
-
-            // Get the multiplier string for the given name
-            String multiplierString = sellMultipliers.getString(name);
-            if (multiplierString == null) {
-                GeneralUtils.logWarning("No multiplier specified for '" + name + "'! Defaulting to 1");
-                return multiplier;
-            }
-
-            try {
-                // Try to parse the multiplier string as a BigDecimal
-                multiplier = BigDecimal.valueOf(Double.parseDouble(multiplierString));
-            } catch (Exception e) {
-                // If the parsing fails, log a warning and return a default multiplier
-                GeneralUtils.logWarning("Invalid multiplier '" + multiplierString + "' specified for '" + name + "'. Defaulting multiplier to 1.");
-                return multiplier;
-            }
-
+        // Check if sell multipliers are configured
+        if (sellMultipliers == null) {
+            GeneralUtils.logWarning("No sell-multipliers specified in config! Defaulting to 1");
             return multiplier;
         }
+
+        // Get the multiplier string for the given name
+        String multiplierString = sellMultipliers.getString(name);
+        if (multiplierString == null) {
+            GeneralUtils.logWarning("No multiplier specified for '" + name + "'! Defaulting to 1");
+            return multiplier;
+        }
+
+        try {
+            // Try to parse the multiplier string as a BigDecimal
+            multiplier = BigDecimal.valueOf(Double.parseDouble(multiplierString));
+        } catch (Exception e) {
+            // If the parsing fails, log a warning and return a default multiplier
+            GeneralUtils.logWarning("Invalid multiplier '" + multiplierString + "' specified for '" + name + "'. Defaulting multiplier to 1.");
+            return multiplier;
+        }
+
+        return multiplier;
     }
 
     // Method to reload the configuration file
